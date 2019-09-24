c03cd790b15d2351d2fe40c70369c46bf3556745
Ethan Painter <ethantpainter@gmail.com>
2019-09-23 20:17:17 -0400

    Adds base css grid logic and removes old router test files

--- src/App.js
+++ src/App.js
@@ -4,28 +4,40 @@ import { Route, Link, BrowserRouter as Router } from "react-router-dom";
 
 import "./App.css";
 
+import Comprehend from "../packages/comprehend/comprehend";
+import ComprehendMedical from "../packages/comprehend-medical/comprehend-medical";
+import Forecast from "../packages/forecast/forecast";
+import ImageRekognition from "../packages/image-rekognition/image-rekognition";
+import Lex from "../packages/lex/lex";
+import Personalize from "../packages/personalize/personalize";
+import Polly from "../packages/polly/polly";
+import Textract from "../packages/textract/textract";
+import Transcribe from "../packages/transcribe/transcribe";
+import Translate from "../packages/translate/translate";
+import VideoRekognition from "../packages/video-rekognition/video-rekognition";
+
 class App extends Component {
   render() {
     return(
       <Router>
-        <div className="App">
+        <div class="app">
 
-          <div id="comprehend">
+          <div class="comprehend">
             <h1 id="comprehend-image">Comprehend Image</h1>
             <Link to="/comprehend">Comprehend</Link>
           </div>
 
-          <div id="comprehend-medical">
+          <div class="comprehend-medical">
             <h1 id="comprehend-medical-image">Comprehend Medical Image</h1>
             <Link to="/comprehend-medical">Comprehend Medical</Link>
           </div>
           
-          <div id="forecast">
+          <div class="forecast">
             <h1 id="forecast-image">Forecast Image</h1>
             <Link to="/forecast">Forecast</Link>
           </div>
 
-          <div id="image-rekognition">
+          <div class="image-rekognition">
             <h1 id="image-rekognition-image">Image Rekognition Image</h1>
             <Link to="/image-rekognition">Image Rekognition</Link>
           </div>
@@ -35,36 +47,47 @@ class App extends Component {
             <Link to="/lex">Lex</Link>
           </div>
 
-          <div id="personalize">
+          <div class="personalize">
             <h1 id="personalize-image">Personalize Image</h1>
             <Link to="/personalize">Personalize</Link>
           </div>
 
-          <div id="polly">
+          <div class="polly">
             <h1 id="polly-image">Polly Image</h1>
             <Link to="/polly">Polly</Link>
           </div>
 
-          <div id="textract">
+          <div class="textract">
             <h1 id="textract-image">Textract Image</h1>
             <Link to="/textract">Textract</Link>
           </div>
 
-          <div id="transcribe">
+          <div class="transcribe">
             <h1 id="transcribe-image">Transcribe Image</h1>
             <Link to="/transcribe">Transcribe</Link>
           </div>
 
-          <div id="translate">
+          <div class="translate">
             <h1 id="translate-image">Translate Image</h1>
             <Link to="/translate">Translate</Link>
           </div>
 
-          <div id="video-rekognition">
+          <div class="video-rekognition">
             <h1 id="video-rekognition-image">Video Rekognition Image</h1>
             <Link to="/video-rekognition">Video Rekognition</Link>
           </div>
         </div>
+        <Route path="/comprehend" component={Comprehend} />
+        <Route path="/comprehend-medical" component={ComprehendMedical} />
+        <Route path="/forecast" component={Forecast} />
+        <Route path="/image-rekognition" component={ImageRekognition} />
+        <Route path="/lex" component={Lex} />
+        <Route path="/personalize" component={Personalize} />
+        <Route path="/polly" component={Polly} />
+        <Route path="/textract" component={Textract} />
+        <Route path="/transcribe" component={Transcribe} />
+        <Route path="/translate" component={Translate} />
+        <Route path="/video-rekognition" component={VideoRekognition} />
       </Router>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,30 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Route, Link, BrowserRouter as Router } from "react-router-dom";
 
 import App from "./App.js";
-import Users from "./users";
-import Contact from "./contact";
 
-const routing = (
-  <Router>
-    <div>
-      <ul>
-        <li>
-          <Link to="/">HOME</Link>
-        </li>
-        <li>
-          <Link to="/users">USERS</Link>
-        </li>
-        <li>
-          <Link to="/contact">CONTACT</Link>
-        </li>
-      </ul>
-      <Route exact path="/" component = {App} />
-      <Route path="/users" component={Users} />
-      <Route path="/contact" component={Contact} />
-    </div>
-  </Router>
-)
-
-ReactDOM.render(routing, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
