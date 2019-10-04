a22b0d79788a69b9d5ea241324195b68f1d6d117
Ethan Painter <ethantpainter@gmail.com>
2019-10-04 08:43:12 -0400

    Adds central js and html for each package

---
+++ src/packages/rekognition/rekognition.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { hot } from "react-hot-loader";
+
+class Rekognition extends React.Component {
+
+  render() {
+    return <h1>Rekognition</h1>
+  }
+}
+
+export default hot(module)(Rekognition);
