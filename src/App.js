49c02df59c6304495bafc7ae524ebd9f4b98a73a
Ethan Painter <ethantpainter@gmail.com>
2019-10-26 16:14:29 -0400

    Adds dark mode button and basic theme setter functions

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import { useLayoutEffect } from 'react';
 
 import { ServiceGrid } from "./landing-page/grid";
 import { Header, Footer, DarkModeButton } from "./shared";
@@ -6,10 +7,15 @@ import { Header, Footer, DarkModeButton } from "./shared";
 import "./css/App.css";
 
 export const App = props => {
+  // Default to Light theme
+  const [themeIndex, setThemeIndex] = useState(0);
+
+  useTheme(themes[themeIndex]);
+
   return (
     <div id="app">
       <div>
-        <DarkModeButton />
+        <DarkModeButton themeSetter={setThemeIndex}/>
         <Header
           title="AWS AI Services"
           mainDescription="Below are a list of Artifical Intelligence (AI) Services provided by
@@ -21,3 +27,28 @@ export const App = props => {
     </div>
   );
 };
+
+// Light and dark themes for the application
+const themes = [
+  // Light
+  {
+    'background': '#ffffff',
+  },
+  // Dark
+  {
+    'background': '#000000',
+  },
+];
+
+export  function useTheme(theme) {
+  useLayoutEffect(
+    () => {
+      // Iterate through each value in theme object
+      for (const key in theme) {
+        // Update css variables in document's root element
+        document.documentElement.style.setProperty(`--${key}`, theme[key]);
+      }
+    },
+    [theme] // Only call again if theme object reference changes
+  );
+}
--- src/shared/button/dark-button.js
+++ src/shared/button/dark-button.js
@@ -3,21 +3,60 @@ import CheckMark from "../../../assets/dark-mode/CheckMark.svg";
 
 import "../../css/shared/dark-mode.css";
 
-export const DarkModeButton = () => {
+export const DarkModeButton = props => {
+  const darkModeTextOptions = {
+    default: "Dark Mode",
+    active: "Active",
+    enable: "Enable",
+    disable: "Disable",
+  };
   const [darkMode, setDarkMode] = useState(false);
+  const [darkModeText, setDarkModeText] = useState(darkModeTextOptions.default);
 
+  // If dark mode is enabled, swap to light
+  // Otherwise swap from light to dark
   const handleDarkModeClick = () => {
     if (darkMode) {
       setDarkMode(false);
-    } else {
+      props.themeSetter(0);
+      setDarkModeText(darkModeTextOptions.active);
+    }
+
+    if (!darkMode) {
       setDarkMode(true);
+      props.themeSetter(1);
+      setDarkModeText(darkModeTextOptions.disable);
+    }
+  };
+
+  const onMouseEnterHandler = () => {
+    if (!darkMode) {
+      setDarkModeText(darkModeTextOptions.enable);
+    }
+
+    if (darkMode) {
+      setDarkModeText(darkModeTextOptions.disable);
+    }
+  };
+
+  const onMouseLeaveHandler = () => {
+    if (!darkMode) {
+      setDarkModeText(darkModeTextOptions.default);
+    }
+
+    if (darkMode) {
+      setDarkModeText(darkModeTextOptions.active);
     }
   };
 
   return (
-    <div id="dark-mode-btn" onClick={handleDarkModeClick}>
-      <h4 id="dark-mode-txt">Dark Mode</h4>
-      {darkMode ? <CheckMark id="check-mark" /> : undefined}
+    <div
+      id="dark-mode-btn"
+      onClick={handleDarkModeClick}
+      onMouseEnter={onMouseEnterHandler}
+      onMouseLeave={onMouseLeaveHandler}
+    >
+      <h5>{darkModeText}</h5>
     </div>
   );
 };
