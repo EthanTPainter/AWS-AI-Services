6ba3b3c17237add6ed31b85b91e3b6147bcf5c20
Ethan Painter <ethantpainter@gmail.com>
2019-11-14 20:52:39 -0500

    Removes storybook from project

--- src/stories/Pages.stories.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import { NotFound } from "../not-found";
-
-export default { title: "Pages" };
-
-export const notFoundPage = () => {
-  return <NotFound />
-}
--- src/stories/dark-mode-btn.stories.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { DarkModeButton } from "../shared/button/dark-button";
-
-export default { title: "DarkModeButton" };
-
-export const darkModeButton = () => (
-  <DarkModeButton />
-);
