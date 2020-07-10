60fc3b3626c5f7daf96482a92689fe1a4b76c6c0
Ethan Painter <ethantpainter@gmail.com>
2020-07-10 15:38:15 -0400

    Adds base logic to handler

--- backend/src/handlers/service-handler.ts
+++ backend/src/handlers/service-handler.ts
@@ -1,19 +1,19 @@
 import * as AWS from "aws-sdk";
 import { APIGatewayEvent, Callback, Context } from "aws-lambda";
+import { processEvent } from "../process/process";
+import { HandlerResult } from "../models/handler-result";
 
 /**
  * This handles aws ai service requests
- * @param event
- * @param context
- * @param callback
+ * @param event API Gateway Event
+ * @param context Lambda Context
+ * @param callback Lambda Callback
  */
 export async function handleServiceRequest(
 	event: APIGatewayEvent,
 	context: Context,
 	callback: Callback
-): Promise<any> {
-	return {
-		statusCode: 200,
-		body: JSON.stringify({ message: "Success" }),
-	};
+): Promise<HandlerResult> {
+	const result = processEvent(event);
+	return result;
 }
