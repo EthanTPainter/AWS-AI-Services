1e7ddba366cce63bc7a311831cf6ea485f743dc4
Ethan Painter <ethantpainter@gmail.com>
2020-07-10 15:39:03 -0400

    Make service name required for lambda event

--- backend/src/lambda-event/event.ts
+++ backend/src/lambda-event/event.ts
@@ -1,14 +1,77 @@
 import { APIGatewayEvent } from "aws-lambda";
 import { RequestEventBody } from "../models/request-event-body";
+import {
+	ComprehendBody,
+	ComprehendMedicalBody,
+	ForecastBody,
+	LexBody,
+	PersonalizeBody,
+	PollyBody,
+	RekognitionBody,
+	TextractBody,
+	TranscribeBody,
+	TranslateBody,
+} from "models/event-bodies";
 
-export const getBody = (event: APIGatewayEvent): RequestEventBody => {
+export const getBody = (
+	event: APIGatewayEvent,
+	serviceName: string
+):
+	| ComprehendBody
+	| ComprehendMedicalBody
+	| ForecastBody
+	| LexBody
+	| PersonalizeBody
+	| PollyBody
+	| RekognitionBody
+	| TextractBody
+	| TranscribeBody
+	| TranslateBody => {
 	if (!event.body) {
 		throw new Error("No body found on the event");
 	}
 
 	try {
-		const body = JSON.parse(event.body);
-		return body;
+		const body = JSON.parse(event.body) as RequestEventBody;
+
+		let selectedServiceBody;
+		switch (serviceName) {
+			case "comprehend":
+				selectedServiceBody = body.comprehend;
+				break;
+			case "comprehend-medical":
+				selectedServiceBody = body.comprehendMedical;
+				break;
+			case "forecast":
+				selectedServiceBody = body.forecast;
+				break;
+			case "lex":
+				selectedServiceBody = body.lex;
+				break;
+			case "personalize":
+				selectedServiceBody = body.personalize;
+				break;
+			case "polly":
+				selectedServiceBody = body.polly;
+				break;
+			case "rekognition":
+				selectedServiceBody = body.rekognition;
+				break;
+			case "textract":
+				selectedServiceBody = body.textract;
+				break;
+			case "transcribe":
+				selectedServiceBody = body.transcribe;
+				break;
+			case "translate":
+				selectedServiceBody = body.translate;
+				break;
+		}
+
+		if (!selectedServiceBody) {
+			throw new Error();
+		}
+		return selectedServiceBody;
 	} catch (error) {
 		throw new Error("Unable to parse event body");
 	}
