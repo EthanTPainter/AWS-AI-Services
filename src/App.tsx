64adb383e61e6747f8079f8f85071dacce6317a4
Ethan Painter <ethantpainter@gmail.com>
2020-10-01 00:26:05 -0400

    Adjust spacing for App file

--- src/App.tsx
+++ src/App.tsx
@@ -8,22 +8,22 @@ import "./App.css";
 import { ButtonBar } from "./shared/button";
 
 type AppProps = {
-  history: History;
+	history: History;
 };
 
 export const App: FunctionComponent<AppProps> = ({ history }) => {
-  return (
-    <div id="app">
-      <div>
-        <ButtonBar hideReturnBtn={true} hideServiceBtn={true} />
-        <Header
-          title="AWS AI Services"
-          mainDescription="Below are a list of Artifical Intelligence (AI) Services provided by
+	return (
+		<div id="app">
+			<div>
+				<ButtonBar hideReturnBtn={true} hideServiceBtn={true} />
+				<Header
+					title="AWS AI Services"
+					mainDescription="Below are a list of Artifical Intelligence (AI) Services provided by
         AWS. Select a service below to see an overview of the service and how to use it."
-        />
-      </div>
-      <ServiceGrid history={history} />
-      <Footer repoUrl="https://www.github.com/EthanTPainter/AWS-AI-Services" />
-    </div>
-  );
+				/>
+			</div>
+			<ServiceGrid history={history} />
+			<Footer repoUrl="https://www.github.com/EthanTPainter/AWS-AI-Services" />
+		</div>
+	);
 };
